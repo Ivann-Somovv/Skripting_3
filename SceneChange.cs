fdfaa6df131110b3e2afb40568a95e5fac45b01c
Ivann-Somovv <152070354+Ivann-Somovv@users.noreply.github.com>
2023-11-26 17:16:44 +0300

    Add files via upload
    eto my 3 D/z

---
+++ SceneChange.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+public string (sceneName);
+        Editor SceneManager.LoadScene(sceneName);
+}
